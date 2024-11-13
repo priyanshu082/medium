7654b76ab67818c803d9fc97e10d2a27eb2770de
priyanshu082 <priyanshusingh216@gmail.com>
2024-11-13 23:55:14 +0530

    Project :: Middleware in backend is done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from 'hono'
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { verify,decode,sign } from 'hono/jwt'
 
 
 //typescript saw errror in env files so there is different way of doing env to string
@@ -11,12 +12,28 @@ const app=new Hono<{
  }
 }>()
 
-app.post('/api/v1/signup', (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-}).$extends(withAccelerate())
-  return c.text('Hello Hono!')
+app.use("/api/v1/blog/*",async (c ,next )=>{
+  const header= c.req.header("authorization") || ""
+const token=header.split(" ")[1]
+  const response =await verify(token,"secret")
+
+  if(response.id){
+    next()
+  } else{
+    c.status(403)
+    return c.json({error:"unauhtorized"})
+  }
 })
+
+    app.post('/api/v1/signup', (c) => {
+
+          const prisma = new PrismaClient({
+          datasourceUrl: c.env.DATABASE_URL,
+          }).$extends(withAccelerate())
+     return c.text('Hello from here!')
+  })
+
+
 app.post('/api/v1/signin', (c) => {
   return c.text('Hello Hono!')
 })
@@ -27,7 +44,7 @@ app.put('/api/v1/blog', (c) => {
   return c.text('Hello Hono!')
 })
 app.get('/api/v1/blog/:id', (c) => {
-  return c.text('Hello Hono!')
+  return c.text('Hello from yoyo here!')
 })
 
 export default app
