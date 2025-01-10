754305adf82631ca205ad37422f66ac17848cbc7
priyanshu082 <priyanshusingh216@gmail.com>
2025-01-10 17:38:30 +0530

    final done

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -24,7 +24,7 @@ export const Appbar = () => {
     // }
     
     return (
-        <div className="border-b flex justify-between px-10 py-4">
+        <div className="border-b flex justify-between px-2 md:px-10 py-4">
             
             <Link to={'/'} className="flex items-center gap-2 text-2xl cursor-pointer">
                 <div className="w-10 h-10 rounded-full bg-orange-500 flex items-center justify-center">
@@ -36,13 +36,13 @@ export const Appbar = () => {
            
             <Link
                 to={'/createRoom'}
-                className="text-black cursor-pointer hover:text-orange-600 hover:scale-110 transition-transform duration-200 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium text-sm px-2 py-2 text-center"
+                className="text-black text-xs sm:text-sm cursor-pointer hover:text-orange-600 hover:scale-110 transition-transform duration-200 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium px-2 py-2 text-center"
             >
                 Create Room
             </Link>
             <Link
                 to={'/bookings'}
-                className="text-black cursor-pointer hover:text-orange-600 hover:scale-110 transition-transform duration-200 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium text-sm px-2 py-2 text-center"
+                className="text-black text-xs sm:text-sm cursor-pointer hover:text-orange-600 hover:scale-110 transition-transform duration-200 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium px-2 py-2 text-center"
             >
                 Bookings
             </Link>
@@ -52,7 +52,7 @@ export const Appbar = () => {
                     navigate('/signin');
                 }}
                 type="button"
-                className="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-2 py-2.5 text-center "
+                className="text-white text-xs sm:text-sm bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full px-2 py-2 text-center"
             >
                 Logout
             </button>
