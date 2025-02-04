95e07db19130eec3dacfa292712d3814b0c17bc7
priyanshu082 <priyanshusingh216@gmail.com>
2025-02-04 10:06:33 +0530

    remove protected route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { Signin } from './pages/Signin'
 import { Home } from './pages/Home'
 import { RoomDetails } from './pages/Room'
